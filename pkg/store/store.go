8daeabbae6630d5a81ca1f3a2f426dd0a9f9c536
vladrosant <pedromuch1@gmail.com>
2025-12-12 20:05:30 +0000

    add pod methods

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -65,3 +65,70 @@ func (s *Store) save() error {
 
 	return os.WriteFile(s.filepath, data, 0644)
 }
+
+func (s *Store) CreatePod(pod api.Pod) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for _, p := range s.state.Pods {
+		if p.Name == pod.Name && p.Namespace == pod.Namespace {
+			return fmt.Errorf("pod %s/%s already exists", pod.Namespace, pod.Name)
+		}
+	}
+
+	s.state.Pods = append(s.state.Pods, pod)
+	return s.save()
+}
+
+func (s *Store) GetPod(namespace, name string) (*api.Pod, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	for _, pod := range s.state.Pods {
+		if pod.Name == name && pod.Namespace == namespace {
+			podCopy := pod
+			return &podCopy, nil
+		}
+	}
+
+	return nil, fmt.Errorf("pod %s/%s not found!", namespace, name)
+}
+
+func (s *Store) ListPods() ([]api.Pod, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	pods := make([]api.Pod, len(s.state.Pods))
+	copy(pods, s.state.Pods)
+	return pods, nil
+}
+
+func (s *Store) UpdatePod(pod api.Pod) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for i, p := range s.state.Pods {
+		if p.Name == pod.Name && p.Namespace == pod.Namespace {
+			s.state.Pods[i] = pod
+			return s.save()
+		}
+	}
+
+	return fmt.Errorf("pod %s/%s not found!", pod.Namespace, pod.Name)
+}
+
+func (s *Store) DeletePod(namespace, name string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for i, pod := range s.state.Pods {
+		if pod.Name == name && pod.Namespace == namespace {
+			s.state.Pods = append(s.state.Pods[:i], s.state.Pods[i+1:]...)
+			return s.save()
+		}
+	}
+
+	return fmt.Errorf("pod %s/%s not found!", namespace, name)
+
+	// add node methods later on (CreateNote, GetNode, ListNodes...)
+}
