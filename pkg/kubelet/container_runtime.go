79d6e88c0ebc2f3437ee7897f4c863408e2c3990
vladrosant <pedromuch1@gmail.com>
2025-12-15 17:56:40 -0300

    add few changes to the runtime code

--- pkg/kubelet/container_runtime.go
+++ pkg/kubelet/container_runtime.go
@@ -43,3 +43,45 @@ func (cr *ContainerRuntime) StartContainer(pod types.Pod) error {
 
 	return nil
 }
+
+func (cr *ContainerRuntime) StopContainer(pod types.Pod) error {
+	containerName := fmt.Sprintf("m8s-%s-%s", pod.Namespace, pod.Name)
+
+	exists, err := cr.containerExists(containerName)
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		return nil
+	}
+
+	cmd := exec.Command("docker", "stop", containerName)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("failed to remove container: %w, output: %s", err, string(output))
+	}
+
+	cmd = exec.Command("docker", "rm", containerName)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("failed to remove container: %w, output: %s", err, string(output))
+	}
+
+	return nil
+}
+
+func (cr *ContainerRuntime) GetContainerStatus(pod types.Pod) (string, error) {
+	containerName := fmt.Sprintf("m8s-%s-%s", pod.Namespace, pod.Name)
+
+	exists, err := cr.containerExists(containerName)
+	if err != nil {
+		return types.PodStatusFailed, err
+	}
+
+	if !exists {
+		return types.PodStatusPending, nil
+	}
+
+	running, err
+
+	return types.PodStatusFailed, nil
+}
